06fcdea2a1249d531f815cc8a9b63b627a877330
BurgBoi <davidwangshark@gmail.com>
2020-08-23 11:54:47 -0400

    added fake server - client connections

--- src/main/java/me/quicksource/controllers/CreateResourceFormController.java
+++ src/main/java/me/quicksource/controllers/CreateResourceFormController.java
@@ -14,53 +14,53 @@ import java.util.ResourceBundle;
 
 public class CreateResourceFormController implements Initializable {
 
-	@FXML
-	private TextField resourceName;
+    @FXML
+    private TextField resourceName;
 
-	@FXML
-	private TextField resourceLink;
+    @FXML
+    private TextField resourceLink;
 
-	public void onCreateResourceClicked(MouseEvent mouseEvent) throws Exception {
+    public void onCreateResourceClicked(MouseEvent mouseEvent) throws Exception {
 
-		if (!isUrlValid("http://" + resourceLink.getText())) {
-			return;
-		}
+        if (!isUrlValid("http://" + resourceLink.getText())) {
+            return;
+        }
 
-		Client client = new Client("127.0.0.1", 5000, data -> {
-		});
+        Client client = new Client("127.0.0.1", 5000, data -> {
+        });
 
-		Button button = new Button(resourceName.getText());
-		button.setOnMouseClicked(event -> {
-			WebEngine engine = TeacherMenuController.teacherWebViewer.getEngine();
-			engine.load("http://" + resourceLink.getText());
-		});
+        Button button = new Button(resourceName.getText());
+        button.setOnMouseClicked(event -> {
+            WebEngine engine = TeacherMenuController.teacherWebViewer.getEngine();
+            engine.load("http://" + resourceLink.getText());
+        });
 
-		JSONObject jsonObject = new JSONObject();
-		jsonObject.put("request_type", "create-resource");
-		jsonObject.put("class", "class"/*i didnt have enough time to do this*/);
-		jsonObject.put("name", resourceName.getText());
-		jsonObject.put("link", resourceLink.getText());
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("request_type", "create-resource");
+        jsonObject.put("class", "class"/*i didnt have enough time to do this*/);
+        jsonObject.put("name", resourceName.getText());
+        jsonObject.put("link", resourceLink.getText());
 
-		client.startConnection();
-		client.sendData(jsonObject.toString());
-		TeacherMenuController.teacherResources.getChildren().add(button);
+        client.startConnection();
+        client.sendData(jsonObject.toString());
+        TeacherMenuController.teacherResources.getChildren().add(button);
 
-		TeacherMenuController.createResourceStage.close();
-		TeacherMenuController.createResourceStage = null;
-		TeacherMenuController.teacherResources = null;
-	}
+        TeacherMenuController.createResourceStage.close();
+        TeacherMenuController.createResourceStage = null;
+        TeacherMenuController.teacherResources = null;
+    }
 
-	private boolean isUrlValid(String url) {
-		try {
-			new URL("http://" + url).toURI();
-			return true;
-		} catch (Exception e) {
-			return false;
-		}
-	}
+    private boolean isUrlValid(String url) {
+        try {
+            new URL("http://" + url).toURI();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
 
-	}
+    }
 }
